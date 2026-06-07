499cd7376883bab0da4e0681988f7ac67e90c7d6
Erfan <erfanroozbahani6@gmail.com>
2026-06-07 18:05:08 +0330

    set base path for Vite configuration to '/props-test/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/props-test/',
   server: {
     port: 3000,
   }
 })
+
+
